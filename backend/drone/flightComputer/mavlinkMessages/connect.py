b5d0c7787687d92ea788038ceef2d9f2603b3d2d
Liam Mah <liam.d.mah@gmail.com>
2026-01-26 15:01:04 -0700

    We now know that the verify_connection is broken. We are trying to figure out why.

--- backend/drone/flightComputer/mavlinkMessages/connect.py
+++ backend/drone/flightComputer/mavlinkMessages/connect.py
@@ -13,14 +13,7 @@ def verify_connection(connection):
 
 def connect_to_vehicle(ip_and_port='udp:127.0.0.1:14550'):
     try:
-        print(f"attempting to connect with ip/port: {ip_and_port}")
         connection = mavutil.mavlink_connection(ip_and_port)
-        print("got connection")
-        # if verify_connection(connection):
-        #     return connection
-        # else:
-        #     print("Not a valid connection. Exiting.")
-        #     exit(1)
         return connection
     except Exception as e:
         print(f"Error: Unable to connect to the vehicle at {ip_and_port}. See error info: {e}")
