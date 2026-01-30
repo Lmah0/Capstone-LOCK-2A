43197a7c749d8fa44647241e61327e66fd7a8a1f
Dominic Gartner <dominic.gartner@ucalgary.ca>
2026-01-30 11:02:42 -0700

    Avoid crash if tracked distance is not yet defined

--- frontend/gcs/src/components/HUD/components/FlightMode.tsx
+++ frontend/gcs/src/components/HUD/components/FlightMode.tsx
@@ -48,7 +48,7 @@ export default function FlightMode({ isMetric, followDistance }: FlightModeProps
                     </Box>
                     <Box className="flex items-center gap-1">
                         <Typography id='dist-to-target' variant="body2" className="text-neutral-300">
-                            Target: {formatUnits.distance(trackingData.distance_to_target, isMetric)}
+                            Target: {formatUnits.distance(trackingData?.distance_to_target, isMetric)}
                         </Typography>
                     </Box>
                 </Box>
